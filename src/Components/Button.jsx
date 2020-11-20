f67714c639f4ff60a5a7c4fffd543887d458fe93
seemasb <seema.sbouh512@gamil.com>
2020-11-21 01:53:41 +0200

    commit after edit

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,28 +1,26 @@
-import React from "react"
+import React from 'react';
 import './Button.css';
-
-
-function Button (props){
-      // const {Action} = props
-      return(
-        <div>
-          <button  onClick={()=>props.myAction(props.children)} className={` ${isNumber(props.children) ? "Button_design" : `${isClear(props.children) ? "light_blue" : "Operator"}` } `}>{props.children}</button>
-        </div>
-      )
-    }
-
-    
-
-
-const isNumber = val => {
-
-    return !isNaN(val) || val === '.'
+function Button(props) {
+  // const {Action} = props
+  return (
+    <div>
+      <button
+        onClick={() => props.myAction(props.children)}
+        className={` ${
+          isNumber(props.children)
+            ? 'Button_design'
+            : `${isClear(props.children) ? 'light_blue' : 'Operator'}`
+        } `}
+      >
+        {props.children}
+      </button>
+    </div>
+  );
 }
-
-const isClear = val => {
-
-    return val==='AC' || val === '+/-' || val === '%'
-}
-
-    export default Button;
-
+const isNumber = (val) => {
+  return !isNaN(val) || val === '.';
+};
+const isClear = (val) => {
+  return val === 'AC' || val === '+/-' || val === '%';
+};
+export default Button;
